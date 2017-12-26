a676ef5c8e10e904cec485f8300664eb7ae17c3f
Pal, Soumitra <soumitra.pal@nih.gov>
2017-12-26 13:07:40 -0500

    created the maps

---
+++ gen_maps.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+df = pd.read_csv("color_map.csv")
+
+primary = df[df['primary']==1]
+secondary = df[df['primary']!=1]
+
+print primary
+print secondary
+
+with open("color_map.R", "w") as f:
+  maps = []
+  for i, row in primary.iterrows():
+    maps.append("'{}' = '{}'".format(row['key'], row['value']))
+  f.write("primary_colors = c(%s)\n" % ',\n\t'.join(maps))
+  maps = []
+  for i, row in secondary.iterrows():
+    maps.append("'{}' = '{}'".format(row['key'], row['value']))
+  f.write("cateogory_colors = c(%s)\n" % ',\n\t'.join(maps))
+
+with open("color_map.tex", "w") as f:
+  maps = []
+  for i, row in primary.iterrows():
+    maps.append("\\def\\col{}{{{}}}".format(row['key'], row['value'][1:]))
+  f.write("%s\n" % '\n'.join(maps))
+  maps = []
+  for i, row in secondary.iterrows():
+    maps.append("\\definecolor\\col{}{{HTML}}{{\\col{}}}".format(row['key'], row['value']))
+  f.write("%s\n" % '\n'.join(maps))
